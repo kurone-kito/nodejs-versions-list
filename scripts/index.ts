df2f675a0bb3c3425795de6e2b35d1312eafb967
kurone-kito <krone@kit.black>
2021-08-06 13:48:08 +0900

    feat: added the hash

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,6 +1,7 @@
 import allNodeVersions from 'all-node-versions';
 import appRootPath from 'app-root-path';
 import { mkdir, writeFile } from 'fs/promises';
+import md5file from 'md5-file';
 import { join } from 'path';
 
 const distDir = join(appRootPath.path, 'dist');
@@ -10,4 +11,5 @@ const distFile = join(distDir, 'index.json');
   const data = JSON.stringify(await allNodeVersions());
   await mkdir(distDir, { recursive: true });
   await writeFile(distFile, data);
+  await writeFile(`${distFile}.md5`, await md5file(distFile));
 })();
