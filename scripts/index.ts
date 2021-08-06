eb9302e816811c73bbd70e85ea1e6cdd77856785
kurone-kito <krone@kit.black>
2021-08-06 13:35:07 +0900

    feat: implemented that versions list put to the JSON

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,10 +1,13 @@
+import allNodeVersions from 'all-node-versions';
 import appRootPath from 'app-root-path';
 import { mkdir, writeFile } from 'fs/promises';
 import { join } from 'path';
 
 const distDir = join(appRootPath.path, 'dist');
+const distFile = join(distDir, 'index.json');
 
 (async () => {
+  const data = JSON.stringify(await allNodeVersions());
   await mkdir(distDir, { recursive: true });
-  await writeFile(join(distDir, 'index.json'), '{}');
+  await writeFile(distFile, data);
 })();
